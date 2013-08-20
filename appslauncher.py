75f2c6a958fcbf0afb2f0fc0012395a8ffed14ed
arundhaj <arundhaj@gmail.com>
2013-08-20 08:47:02 +0530

    printing key

--- appslauncher.py
+++ appslauncher.py
@@ -1,7 +1,8 @@
-# Whenever I start my machine, I wanted these applications and files to be loaded.
+# Whenever I start my machine, I wanted these applications &files to be loaded.
 # This script works on Windows 7 machine
 
-import os, sys
+import os
+import sys
 import webbrowser
 import yaml
 
@@ -14,26 +15,29 @@ stream = open(configfile, 'r')
 conf = yaml.load(stream)
 
 for key in conf:
-	if key == "file":
-		for file in conf[key]:
-			try: 
-				os.startfile(file)
-			except:
-				print 'Failed to open', file
-				print sys.exc_info()[0]
-	elif key == "app":
-		for app in conf[key]:
-			try:
-				os.startfile(app)
-			except:
-				print 'Failed to open', app
-				print sys.exc_info()[0]
-	elif key == "uri":
-		for uri in conf[key]:
-			try:
-				webbrowser.open(uri)
-			except: 
-				print 'Failed to open', uri
-				print sys.exc_info()[0]
-	else:
-		print "nothing"
+    if key == "file":
+        for file in conf[key]:
+            try:
+                print 'Opening', file
+                os.startfile(file)
+            except:
+                print 'Failed to open', file
+                print sys.exc_info()[0]
+    elif key == "app":
+        for app in conf[key]:
+            try:
+                print 'Starting', app
+                os.startfile(app)
+            except:
+                print 'Failed to open', app
+                print sys.exc_info()[0]
+    elif key == "uri":
+        for uri in conf[key]:
+            try:
+                print 'Opening', uri
+                webbrowser.open(uri)
+            except:
+                print 'Failed to open', uri
+                print sys.exc_info()[0]
+    else:
+        print "nothing"
